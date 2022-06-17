55c7e8f7bef64f6d764912ec0d23ff866a48b2d8
aldoherzac <zac@ciencias.unam.mx>
2022-06-17 16:16:26 -0500

    [FIXUP]: Compiling succesful.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './infrastructure/ui/web/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
